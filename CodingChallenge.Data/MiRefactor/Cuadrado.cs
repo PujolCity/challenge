0aad1f4e35dff7a8997a7b5247fdaf5f2839be87
sergio <sergio19101992@gmail.com>
2023-06-15 17:53:10 -0300

    Se implementa la interfaz IFormaGeometrica para poder implementar luego en las clases concretas y asi implementar en cada clase su propia formula de area y perimetro

---
+++ CodingChallenge.Data/MiRefactor/Cuadrado.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenge.Data.MiRefactor
+{
+    public class Cuadrado : IFormaGeometrica
+    {
+        private readonly decimal _lado;
+        public Cuadrado(decimal lado)
+        {
+            _lado = lado;
+        }
+        public decimal CalcularArea()
+        {
+            return _lado * _lado;
+        }
+
+        public decimal CalcularPerimetro()
+        {
+            return _lado * 4;
+        }
+    }
+}
---
+++ CodingChallenge.Data/MiRefactor/Trapecio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenge.Data.MiRefactor
+{
+    // TRAPECIO ISOSCELES 
+    public class Trapecio : IFormaGeometrica
+    {
+        private readonly decimal _baseMayor;
+        private readonly decimal _baseMenor;
+        private readonly decimal _altura;
+
+        public Trapecio(decimal baseMayor, decimal baseMenor, decimal altura)
+        {
+            _baseMayor = baseMayor;
+            _baseMenor = baseMenor;
+            _altura = altura;
+        }
+
+        public decimal CalcularArea()
+        {
+            return ((_baseMayor + _baseMenor) / 2) * _altura;
+        }
+
+        public decimal CalcularPerimetro()
+        {
+            decimal ladoOblicuo = CalcularLadoOblicuo();
+            return _baseMayor + _baseMenor + (ladoOblicuo * 2);
+        }
+
+        private decimal CalcularLadoOblicuo()
+        {
+            decimal diferenciaBases = (_baseMayor - _baseMenor) / 2;
+            decimal ladoOblicuo = (decimal)Math.Sqrt((double)((diferenciaBases * diferenciaBases) + (_altura * _altura)));
+            return ladoOblicuo;
+        }
+    }
+}
